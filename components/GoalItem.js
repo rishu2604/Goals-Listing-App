e97d25ffb74432cfdeb0c5ab76d4f0478665a0c9
Rishu Anand <rishu.anand@knowmax.ai>
2024-10-25 13:07:15 +0530

    feat: add goal image, status bar and changed css

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,23 +1,30 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 
 export default function GoalItem(props) {
-    return (
-        <View style={styles.goalItem}>
-            <Text style={styles.goalText}>
-                {props.text}
-            </Text>
-        </View>
-    );
+  return (
+    <View style={styles.goalItem}>
+      <Pressable
+        onPress={props.onDelete.bind(this, props.id)}
+        android_ripple={{ color: "#210644" }}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
+        <Text style={styles.goalText}>{props.text}</Text>
+      </Pressable>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    goalItem: {
-        margin: 8,
-        backgroundColor: "#5e0acc",
-        borderRadius: 6,
-        padding: 8,
-    },
-    goalText: {
-        color: "white",
-    }
-})
+  goalItem: {
+    margin: 8,
+    backgroundColor: "#5e0acc",
+    borderRadius: 6,
+  },
+  goalText: {
+    color: "white",
+    padding: 8,
+  },
+  pressedItem: {
+    opacity: 0.5,
+  },
+});
